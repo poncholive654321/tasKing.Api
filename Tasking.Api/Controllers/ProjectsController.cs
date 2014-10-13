ea929d3600beabd98964bf150adfe06d8d23a0cd
poncholive654321 <luisperdomo1985@live.com>
2014-10-13 20:51:28 -0300

    second publish

---
+++ Tasking.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Tasking.Api.Models;
+
+namespace Tasking.Api.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class ProjectsController : ApiController
+    {
+        [Authorize]
+        public async Task<IHttpActionResult> Get()
+        {
+            var n = this.User.Identity.Name;
+            return Ok(await ProjectModel.DummyProjects());
+        }
+    }
+}
