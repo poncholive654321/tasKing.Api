f4db7281a4f0ce1a5aa75e24a5a6f65efb5c202e
poncholive654321 <luisperdomo1985@live.com>
2014-10-13 20:49:12 -0300

    aaaaaadasd

---
+++ Tasking.Web/app/controllers/ordersController.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+app.controller('ordersController', ['$scope', 'ordersService', function ($scope, ordersService) {
+
+    $scope.orders = [];
+
+    ordersService.getOrders().then(function (results) {
+
+        $scope.projects = results.data;
+
+    }, function (error) {
+        //alert(error.data.message);
+    });
+
+}]);
