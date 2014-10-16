a0d1dc01a3514b2ea424ff6f946faa2ec7c93cd4
poncholive654321 <luisperdomo1985@live.com>
2014-10-16 07:20:07 -0300

    Iniciado servicio de proyectos

---
+++ Tasking.Web/app/services/projectsService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('projectsService', ['$http', function ($http) {
+
+    var serviceBase = 'http://localhost:52509/';
+    var projectsServiceFactory = {};
+
+    var _getAllProjectsByUser = function () {
+
+        return $http.get(serviceBase + 'api/projects').then(function (results) {
+            return results;
+        });
+    };
+
+    ordersServiceFactory.getAllProjectsByUser = _getProjectsByUser;
+
+    return projectsServiceFactory;
+
+}]);
